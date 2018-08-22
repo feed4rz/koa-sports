b7bb8d16082fc4ada5ed76b9eb1316fee7ee45c0
feed4rz <mrspanky@yandex.ru>
2018-08-22 15:49:57 +0300

    Removed defaultScope + added team_id to schema

--- models/player.js
+++ models/player.js
@@ -3,11 +3,8 @@ const type = require('sequelize')
 
 const Player = global.sq.define('player', {
   id: { autoIncrement: true, primaryKey: true, type: type.INTEGER },
-  name: type.STRING
-}, {
-  defaultScope: {
-    raw: true
-  }
+  name: type.STRING,
+  team_id: type.INTEGER
 })
 
 module.exports = Player
