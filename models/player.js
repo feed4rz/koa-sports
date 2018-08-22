34b1bc62b5119473a7cca9e2fea8aec4d89175c6
feed4rz <mrspanky@yandex.ru>
2018-08-22 15:18:45 +0300

    Player model

---
+++ models/player.js
@@ -0,0 +1,12 @@
+const type = require('sequelize')
+
+const Player = global.sq.define('player', {
+  id: { autoIncrement: true, primaryKey: true, type: type.INTEGER },
+  name: type.STRING
+}, {
+  defaultScope: {
+    raw: true
+  }
+})
+
+module.exports = Player
